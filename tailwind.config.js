88b52922edaa9e3774fd1a9bccc78114846fb7f6
Abhay2412 <abhaykhosla0@gmail.com>
2023-04-14 16:12:11 -0600

    Add in custom colors and screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: "490px",
+      md: "770px",
+      lg: "980px",
+      xl: "140px"
+    },
+    extend: {
+      colors: {
+        brightRed: "#EE4B2B",
+        brightRedLight: "#FF9999",
+        brightRedSuperLight: "#FFE6E6",
+        darkBlue: "000099",
+        darkGreyishBlue: "#424952",
+        veryDarkBlue: "#00004D",
+        paleRed: "#D9544D",
+        lightGrey: "#D3D3D3"
+
+      }
+    },
   },
   plugins: [],
 }
